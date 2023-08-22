374489a77ac947dfda29fe4f0e92142972043570
SajeelHaiderBlinder <sajeelblinder@gmail.com>
2023-08-22 09:51:52 +0500

    * added join page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import { Header } from "./Frontend/Pages/Home/Header/Header";
 import { Login } from "./Frontend/Pages/Login/Login";
 import { Signup } from "./Frontend/Pages/Signup/Signup";
+import { Join } from "./Frontend/Pages/Join/Join";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path="/aboutus" element={<Header />} />
+        <Route path="/join" element={<Join />} />
         <Route path="/howitworks" element={<Header />} />
         <Route path="/services" element={<Header />} />
         <Route path="/login" element={<Login />} />
